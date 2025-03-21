ef727a9be01d98ad7c377cf137d867c4e18ff856
BenjaminMiddleton <benjamin.middleton@mail.bcu.ac.uk>
2025-03-21 04:57:57 +0000

    Remove dist submodule reference and add dist to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Craig_Nav/',  // This should match your repository name
+  base: '/Webinar/',  // This should match your repository name
 });
